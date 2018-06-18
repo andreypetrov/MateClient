824ff5d2ddabab1b8a98d1cb1bef57859e4fd2a3
ANDREY\Andrey <petrov.development@gmail.com>
2018-06-18 19:00:58 -0400

    added a sample component

---
+++ src/components/common/Input.vue
@@ -0,0 +1,26 @@
+<template>
+  <input class="form-control mb-2"
+         type="text"
+         :placeholder="pholder"
+         :value="text"
+         v-on:input="input"/>
+</template>
+
+<script>
+  export default {
+    name: 'mt-input',
+    methods: {
+      input(event) {
+        this.$emit('input', event.target.value);
+      },
+    },
+    props: {
+      pholder: String,
+      text: String,
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
--- src/components/students/AddStudent.vue
+++ src/components/students/AddStudent.vue
@@ -1,11 +1,33 @@
 <template>
-    $END$
+  <div class="base-container">
+    <h3 class="bg-secondary p-3">Добави ученик</h3>
+    <div class="bg-light p-3">
+      <mt-input pholder="Име" v-model="name"></mt-input>
+      <mt-input pholder="Клас" v-model="group"></mt-input>
+      <mt-input pholder="Аватар" v-model="avatar"></mt-input>
+      <mt-button title="Добави"></mt-button>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "AddStudent"
-    }
+  import MtButton from '../common/Button';
+  import MtInput from '../common/Input';
+
+  export default {
+    name: 'mt-add-student',
+    data() {
+      return {
+        name: '',
+        group: '',
+        avatar: '',
+      };
+    },
+    components: {
+      MtInput,
+      MtButton,
+    },
+  };
 </script>
 
 <style scoped>
